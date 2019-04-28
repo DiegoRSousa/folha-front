478222070f5d272d15a80d5a7bfe328b2aee5f14
Diego <diegors90@gmail.com>
2019-04-28 19:32:34 -0300

    first commit

---
+++ src/common/template/dependencies.js
@@ -0,0 +1,14 @@
+import './jquery'
+
+import 'admin-lte/plugins/jQueryUI/jquery-ui'
+import 'admin-lte/plugins/fastclick/fastclick'
+import 'admin-lte/plugins/slimScroll/jquery.slimscroll'
+import 'admin-lte/dist/js/app.min'
+
+import 'font-awesome/css/font-awesome.min.css'
+import 'ionicons/dist/css/ionicons.min.css'
+import 'admin-lte/bootstrap/css/bootstrap.min.css'
+import 'admin-lte/dist/css/skins/_all-skins.css'
+import 'admin-lte/plugins/iCheck/flat/blue.css'
+
+import './custom.css'
---
+++ src/main/App.jsx
@@ -0,0 +1,8 @@
+import '../common/template/dependencies'
+import React from 'react';
+
+export default porps => (
+    <div className="wrapper">
+        <h1>Hello World!</h1>
+    </div>
+);
